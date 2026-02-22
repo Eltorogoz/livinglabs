1a34d0be95f2c737ab0aed67402cfbb7910ca4c9
Shane Eversole <eversoleshane7@gmail.com>
2026-02-22 00:49:15 -0500

    started the Help page

---
+++ src/pages/Help.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+
+function Help() {
+    return(
+        <div className="min-h-screen bg-white font-sans text-gray-900">
+
+            <Header />
+
+
+
+
+
+        </div>
+    )
+
+
+};
+export default Help;
