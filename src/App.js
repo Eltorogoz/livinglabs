1387b529259aa8c594500fd73a59d3415addf2f4
Eltorogoz <156124205+Eltorogoz@users.noreply.github.com>
2026-02-27 10:35:44 -0500

    Add Help route to App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Documents from "./pages/Documents";
 import Faq from "./pages/FAQ";
+import Help from "./pages/Help";
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/documents" element={<Documents />} />
-      <Route path="/faq" element={<Faq />} />
+      <Route path="/faq" element={<Faq />} /> 
+      <Route path="/help" element={<Help />} />
     </Routes>
   );
 }
